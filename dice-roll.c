767b1975ad8beb151f0852e68c7abe6883e38852
MichalObi <ndwz123@gmail.com>
2017-12-13 20:53:37 +0100

    Some simplicity of c code + coments

--- dice-roll.c
+++ dice-roll.c
@@ -1,13 +1,20 @@
+/* headers files */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <emscripten/emscripten.h>
 
-int main(int argc, char ** argv) {
+/** function fire on load = argc is size
+of parameter pass in run, char* is pointer to argument variables **/
+
+int main(int argc, char* argv[]) {
     printf("WebAssembly module loaded\n");
 }
 
 int EMSCRIPTEN_KEEPALIVE roll_dice() {
-    srand ( time(NULL) );
+    /* By using time as seed for srand we sure,
+    that the next roll will be diff then previous one*/
+
+    srand (time(NULL));
     return rand() % 6 + 1;
 }

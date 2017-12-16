c3a2740b6a82db8ebe8cbf6653e702cbb96ba364
MichalObi <ndwz123@gmail.com>
2017-12-16 20:29:41 +0100

    classic fibonacci function

--- webAssembly-playground.c
+++ webAssembly-playground.c
@@ -8,13 +8,26 @@
 of parameter pass in run, char* is pointer to argument variables **/
 
 int main(int argc, char* argv[]) {
-    printf("WebAssembly module loaded\n");
+    printf("WebAssembly dice module loaded\n");
 }
 
 int EMSCRIPTEN_KEEPALIVE roll_dice() {
     /* By using time as seed for srand we sure,
-    that the next roll will be diff then previous one*/
-
+    that the next roll will be more random */
     srand (time(NULL));
     return rand() % 6 + 1;
+}
+
+// n tell us how many times accumulator wil be extended
+int EMSCRIPTEN_KEEPALIVE classic_fibonacci(int n){
+  int a = 1;
+  int b = 1;
+
+  while (n-- > 1) {
+    int t = a;
+    a = b;
+    b += t;
+  }
+
+  return b;
 }
